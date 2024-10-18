8867b2419e1ba000b700f42ccce5c26fdf1069c1
Sebastian Nagel <sebastian.nagel@uni-konstanz.de>
2024-10-18 13:01:39 +0200

    Verify downloads using provided SHA1 checksum file

--- src/script/sor-download.sh
+++ src/script/sor-download.sh
@@ -29,35 +29,43 @@ TARGET_DIRECTORY="$1"; shift || { help; exit 1; }
 START="$1"
 DAYS="${2:-1}"
 
+BASE_URL="https://dsa-sor-data-dumps.s3.eu-central-1.amazonaws.com/"
+
+MAX_RANDOM_WAIT=157
+
+
 function LOG__() {
     echo $(date '+[%Y-%m-%d %H:%M:%S]') "$@"
 }
 
+
 LOG__ "Downloading EU DSA Transparency DB SoR daily archives..."
 LOG__ "start = $START, days = $DAYS, target directory = $TARGET_DIRECTORY"
-
-MAX_RANDOM_WAIT=157
 
 if [ -z "$START" ]; then
     # default: fetch data from two days ago, because yesterday's data may not be yet ready
     START=$(date --date="yesterday yesterday" '+%Y-%m-%d')
 fi
 
-BASE_URL="https://dsa-sor-data-dumps.s3.eu-central-1.amazonaws.com/"
-
 DATE=$START
 for i in $(seq 1 $DAYS); do
+
     if [ $i -gt 1 ]; then
         SLEEP=$(($RANDOM%$MAX_RANDOM_WAIT))
         LOG__ "Waiting $SLEEP seconds before downloading $DATE"
         sleep $SLEEP
     fi
+
     TARGET_LOC="$TARGET_DIRECTORY/year=$(date --date="$DATE" '+%Y')/month=$(date --date="$DATE" '+%m')/day=$(date --date="$DATE" '+%d')"
     LOG__ "Downloading $DATE to $TARGET_LOC"
     mkdir -p "$TARGET_LOC"
+
     cd "$TARGET_LOC"/
     wget --no-verbose --timestamping "$BASE_URL"sor-global-$DATE-full.zip
+    wget --no-verbose --timestamping "$BASE_URL"sor-global-$DATE-full.zip.sha1
+    sha1sum --check sor-global-$DATE-full.zip.sha1
     cd -
+
     DATE=$(date --date="$DATE next day" '+%Y-%m-%d')
 done
 
